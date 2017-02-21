7795efd0805c530cbf612b79beb4e10d189e747f
Bret Combast <bret_combast@ultimatesoftware.com>
2017-02-21 16:59:30 -0500

    Uhm, I wrote most of the blog with only 1 commit. Oops.

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,7 @@ Route::get('/', function () {
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login');
 Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+Route::get('logout', 'Auth\LoginController@logout')->name('logout');
 
 // Password Reset Routes...
 Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
