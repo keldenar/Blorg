9e02eebcd6995efe9e82c51c79becc28a3381644
Bret Combast <bret_combast@ultimatesoftware.com>
2017-02-23 14:26:34 -0500

    Removed it.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,7 +24,7 @@
     <style>
         html, body {
             background-color: #fff;
-            color: #636b6f;
+            color: #31393d;
             font-family: 'Raleway', sans-serif;
             font-weight: 100;
             height: 100vh;
